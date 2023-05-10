5d6714c29aa9d9c55b98fb647f0d13b2c29d64f1
Subashgiri7 <125265594+Subashgiri7@users.noreply.github.com>
2023-05-10 11:24:04 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// /**typing */
+// // var typed =new Typed(".typing",{
+// //     Strings:["web designer","Web Designer","Web Developer","Student"],
+// //     typeSpeed:100,
+// //     Backspeed:60,
+// //     loop:true
+// // })
+
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Student","Gymer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+function showAlert() {
+    alert("Hello, world!");
+  }
+  <button type="submit" class="btn">Send Message</button>
+  
